e8673e50ac2559d8940e5866395ecfedac98c66f
ethornbury <thornburye@gmail.com>
2021-07-08 15:12:26 +0100

    added about home contact static page controller

---
+++ app/controllers/static_page_controller.rb
@@ -0,0 +1,13 @@
+class StaticPageController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+  
+  #added this after generating the controller
+  #need to also create the relevant view about.html.erb in the views>static_page
+  
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  #get 'static_page/home'
+  #get 'static_page/contact'
+  #get 'static_page/about'
+  
+  get 'about', to: 'static_page#about'
+  get 'contact', to: 'static_page#contact'
+  root to: 'static_page#home'  #root 
+  
   resources :activities
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
